f1146ade05fc737803792b3bea71685866208018
Henry Catalini Smith <henry@catalinismith.se>
2025-01-03 20:05:05 +0100

    Install TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
   content: ["./index.njk", "./_includes/*.njk"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+} satisfies Config
