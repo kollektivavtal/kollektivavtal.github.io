a6a53d2db7469273e4f96d42c1eca3c665f7eaaa
Henry Catalini Smith <henry@catalinismith.se>
2025-01-03 14:53:16 +0100

    Remove Sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 export default {
   content: ["./index.njk", "./_includes/*.njk"],
   theme: {
-    extend: {
-      colors: {
-        bgStart: 'rgba(5, 9, 15, 1)', // Corresponds to rgb(5,9,15)
-        bgEnd: 'rgba(10, 19, 44, 1)', // Corresponds to rgba(10,19,44,1)
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
