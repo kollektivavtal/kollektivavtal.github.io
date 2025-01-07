1aeca06e013933f6a57473f1f1fb5cca9fea23f3
Henry Catalini Smith <henry@catalinismith.se>
2025-01-07 20:24:57 +0100

    Add agreement count

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,3 +1,17 @@
 export default async function (eleventyConfig) {
-  //
+  eleventyConfig.addCollection("years", async function () {
+    return Promise.all(
+      ["2024", "2023", "2021", "2020", "2017"].map(async (year) => {
+        const url = `https://kollektivavtal.github.io/${year}/sitemap.xml`;
+        const response = await fetch(url);
+        const text = await response.text();
+        const agreementCount = (text.match(/<url>/g) || []).length;
+        console.log(`Year ${year} has ${agreementCount} documents`);
+        return {
+          name: year,
+          agreementCount,
+        };
+      }),
+    );
+  });
 }
